7d8ebea6ac48c447ab73f30693f119b8c0fac8b6
Shang-Hung (Sean) Tsai <shtsai7@users.noreply.github.com>
2018-04-03 09:43:53 -0400

    add dark squares

--- BoardGUI.py
+++ BoardGUI.py
@@ -28,13 +28,20 @@ class BoardGUI():
         self.c.pack()
         self.board = [[0]*self.COLS for _ in range(self.ROWS)]
         self.tiles = [[None for _ in range(self.COLS)] for _ in range(self.ROWS)]
-        self.updateBoard()
+
+        # Print dark square
+        for i in range(6):
+            for j in range(6):
+                if (i + j) % 2 == 1:
+                    self.c.create_rectangle(i * self.row_height, j * self.col_width,
+                                        (i+1) * self.row_height, (j+1) * self.col_width, fill="gray", outline="gray")
 
         # Print grid lines
         for i in range(6):
             self.c.create_line(0, self.row_height * i, self.WINDOW_WIDTH, self.row_height * i, width=2)
             self.c.create_line(self.col_width * i, 0, self.col_width * i, self.WINDOW_HEIGHT, width=2)
 
+        self.updateBoard()
 
         # Initialize parameters
         self.checkerSelected = False
@@ -70,6 +77,9 @@ class BoardGUI():
                             self.tiles[i][j] = self.c.create_oval(j*self.col_width+10, i*self.row_height+10,
                                                                   (j+1)*self.col_width-10, (i+1)*self.row_height-10,
                                                                   fill="red")
+                        else:  # no checker
+                            continue
+                        self.c.tag_raise(self.tiles[i][j])
             self.game.completeBoardUpdate()
         self.root.after(1000, self.updateBoard)
 
